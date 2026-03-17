ed7a85e4c9e625409ee46d3c3472c92af261e974
Vinicius <vtorg.martins@gmail.com>
2026-03-17 10:19:51 -0300

    começando a usar o django admin e registrando categoryes

--- recipes/models.py
+++ recipes/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=65)
+    def __str__(self):
+        return self.name
+
+    
 
 
 class Recipe(models.Model):
