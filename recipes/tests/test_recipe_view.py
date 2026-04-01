3c5e373dc9a4220968c57ee9c13a58593ce710c7
Vinicius <vtorg.martins@gmail.com>
2026-04-01 10:27:12 -0300

    a

--- recipes/tests/test_recipe_view.py
+++ recipes/tests/test_recipe_view.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
+from recipes.models import Category, Recipe, User
 # Create your tests here.
 
 class RecipeViewTest(TestCase):
@@ -8,10 +9,73 @@ class RecipeViewTest(TestCase):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func,  views.home)
 
+    def test_recipe_home_view_returns_status_code_200_ok(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_template_shows_no_recie_found_if_no_recipe(self):
+        response = self.client.get(reverse("recipes:home"))
+        self.assertIn(
+            '<h1>Nenhuma receita encontrada</h1>',
+            response.content.decode('utf-8')
+        )
+
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
         self.assertIs(view.func,  views.category)
 
+    def test_recipe_category_view_returns_404_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:category', kwargs= {'category_id': 1000}))
+        self.assertEqual(response.status_code, 404)
+
+
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name= "category")
+        user = User.objects.create_user(
+            first_name = 'first',
+            last_name = 'name',
+            username = 'user',
+            password = '123456',
+            email = '123@gmail.com',
+        )
+        recipe = Recipe.objects.create(
+            category= category,
+            author= user,
+            tittle= 'Recipe Tittle',
+            description= 'RecipeRecipeRecipeRecipeRecipeRecipe',
+            slug= 'Recipe=slug',
+            preparation_time= 10,
+            preparation_time_unit= 'Minutos',
+            sevings= 4,
+            sevings_unit= 'Pessoas',
+            preparation_steps= 'reciperecipereciperecipereciperecipereciperecipereciperecipe',
+            preparation_steps_is_html= False,
+            is_published= True ,
+        )
+        response = self.client.get(reverse('recipes:home'))
+        response_recipes = response.context['recipes']
+        self.assertEqual(response_recipes.first().tittle, 'Recipe Tittle')
+
+
+        response_context = response.content.decode('utf-8')
+        self.assertIn('Recipe Tittle', response_context)
+        self.assertIn('10 Minutos', response_context)
+        self.assertIn('4 Pessoas', response_context)
+        self.assertEqual(len(response_recipes),1)
+    
+
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func,  views.recipe)
+
+    def test_recipe_detail_view_returns_404_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:recipe', kwargs= {'id': 1000}))
+        self.assertEqual(response.status_code, 404)
+
+
+
+        
