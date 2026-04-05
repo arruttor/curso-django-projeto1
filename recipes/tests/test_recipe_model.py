4bdb33d76e4d45748c85cc9502611ebda3e9a22a
Vinicius <vtorg.martins@gmail.com>
2026-04-05 10:08:46 -0300

    colocando parameterized para testes com mais de um parametro

--- recipes/tests/test_recipe_model.py
+++ recipes/tests/test_recipe_model.py
@@ -1,6 +1,8 @@
 from recipes.tests.test_recipe_base import RecipeTestBase
 from django.core.exceptions import ValidationError
 
+from parameterized import parameterized
+
 class RecipeModelTest(RecipeTestBase):
     def setUp(self):
         self.recipe = self.make_recipe()
@@ -12,3 +14,17 @@ class RecipeModelTest(RecipeTestBase):
 
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
+
+
+    @parameterized.expand([
+            ('tittle', 65),
+            ('description', 165),
+            ('preparation_time_unit', 65),
+            ('sevings_unit', 65),
+        ])
+    def test_recipe_fields_max_length(self,field,max_length):
+        setattr(self.recipe, field, "A" * (max_length+1))
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
+
+ 
